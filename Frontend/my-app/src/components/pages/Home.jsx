b680915fb68f34ee36eb25cceec78924882f717f
Nikhil Kushwaha <osndl.nikhilk@aicofindia.com>
2025-05-23 17:54:08 +0530

    updated frontend

--- Frontend/my-app/src/components/pages/Home.jsx
+++ Frontend/my-app/src/components/pages/Home.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
 
 const Home = () => {
+  const [apidata,setApidata] = useState(null);
+  useEffect(()=>{
+    axios.get("http://localhost:3000/")
+    .then((res)=>{
+      const data = res.data;
+      setApidata(data);
+    })
+
+    useEffect(()=>{
+      console.log("this is api data",apidata);
+
+    },[apidata]);
+  })
   return (
     <div>Home</div>
   )
